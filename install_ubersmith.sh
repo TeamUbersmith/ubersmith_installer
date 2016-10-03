a02ad66072c8f9421fc159282712d93e6c5d29a1
Michael Styne <mstyne@ubersmith.com>
2016-10-03 16:22:42 -0400

    - updates post centos 7 test - pip may not be available - try to install with easy_install - install docker-compose - update templates - update password vars

--- install_ubersmith.sh
+++ install_ubersmith.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing Ansible..."
-pip install ansible -qU --user
+# SSL and Python development libraries are required.
+# sudo yum install gcc libffi-devel python-devel openssl-devel
+# sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+
+echo "Checking for pip..."
+if command -v pip >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    pip install ansible -qU --user
+else
+    if command -v easy_install 2>/dev/null; then
+        easy_install --user pip
+    else
+        echo "Both easy_install and pip are missing; please install pip."
+        echo "https://pip.pypa.io/en/stable/installing/"
+        exit 1
+    fi
+    echo "Installing Ansible..."
+    pip install ansible -qU --user
+fi
 echo "Installing Ubersmith..."
 ansible-playbook -i ./hosts -c local install_ubersmith.yml
