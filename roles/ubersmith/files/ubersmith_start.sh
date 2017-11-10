161bc586075a8055e1f80b103f5c88cc8538b573
Michael Styne <mstyne@ubersmith.com>
2017-11-10 16:19:24 -0500

    - use updated compose commands

--- roles/ubersmith/files/ubersmith_start.sh
+++ roles/ubersmith/files/ubersmith_start.sh
@@ -3,6 +3,5 @@
 # Stops, removes, and recreates selected base Ubersmith containers.
 
 cd /usr/local/ubersmith
-docker-compose -p ubersmith stop
-docker-compose -p ubersmith rm -f
+docker-compose -p ubersmith rm -f -s
 docker-compose -p ubersmith up -d cron db mail php solr web
