4a4fda9745c03e5cfbff049295fb4a7a04e01661
Michael Styne <mstyne@ubersmith.com>
2018-01-09 11:47:34 -0500

    - remove unneeded 'cd' commands

--- roles/ubersmith/files/ubersmith_start.sh
+++ roles/ubersmith/files/ubersmith_start.sh
@@ -2,6 +2,5 @@
 #
 # Stops, removes, and recreates selected base Ubersmith containers.
 
-cd /usr/local/ubersmith
 docker-compose -p ubersmith rm -f -s
 docker-compose -p ubersmith up -d cron db mail php solr web
