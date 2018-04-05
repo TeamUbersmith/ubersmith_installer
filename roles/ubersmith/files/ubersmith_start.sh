28275135d0c32792cdfa168cc5018ba328f907ef
Michael Styne <mstyne@ubersmith.com>
2018-04-05 11:08:10 -0400

    - by default only start one redis container

--- roles/ubersmith/files/ubersmith_start.sh
+++ roles/ubersmith/files/ubersmith_start.sh
@@ -3,5 +3,5 @@
 # Stops, removes, and recreates selected base Ubersmith containers.
 
 docker-compose -p ubersmith rm -f -s
-docker-compose -p ubersmith up --scale redis=3 -d redis
+docker-compose -p ubersmith up --scale redis=1 -d redis
 docker-compose -p ubersmith up -d cron db mail php solr web
