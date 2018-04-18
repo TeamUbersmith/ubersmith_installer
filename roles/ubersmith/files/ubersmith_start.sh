2e9f6afa292381ec91939a7faa2699cd62f86927
Michael Styne <mstyne@ubersmith.com>
2018-04-18 13:54:42 -0400

    - copy ubersmith_start - it's not a cluster, it's a group

--- roles/ubersmith/files/ubersmith_start.sh
+++ roles/ubersmith/files/ubersmith_start.sh
@@ -2,7 +2,7 @@
 #
 # Recreates selected base Ubersmith containers.
 
-REDIS_CLUSTER_SIZE=1
+REDIS_SCALE_SIZE=1
 
-docker-compose -p ubersmith up --scale redis=$REDIS_CLUSTER_SIZE -d redis
+docker-compose -p ubersmith up --scale redis=$REDIS_SCALE_SIZE -d redis
 docker-compose -p ubersmith up -d cron db mail php solr web 
