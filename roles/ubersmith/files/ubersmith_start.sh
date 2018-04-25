b7d2d7e3319b00189f018b5298f7b58ba20d171c
Michael Styne <mstyne@ubersmith.com>
2018-04-25 15:43:36 -0400

    - fix ubersmith_start.sh

--- roles/ubersmith/files/ubersmith_start.sh
+++ roles/ubersmith/files/ubersmith_start.sh
@@ -2,7 +2,5 @@
 #
 # Recreates selected base Ubersmith containers.
 
-REDIS_SCALE_SIZE=1
-
-docker-compose -p ubersmith up --scale redis=$REDIS_SCALE_SIZE -d redis
 docker-compose -p ubersmith up -d cron db mail php solr web 
+docker-compose -p ubersmith up --scale redis=1 -d redis
