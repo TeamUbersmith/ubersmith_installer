d5aa7385224a5f27d35756f463cc12f735fb0fb2
Michael Styne <mstyne@ubersmith.com>
2018-08-01 10:57:36 -0400

    - we should probably install ansible before we try to use it

--- configure.sh
+++ configure.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+export PYTHONUSERBASE="$HOME/.local"
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "Checking for pip..."
+if command -v pip >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    pip install ansible -q --upgrade --user
+else
+    if command -v easy_install 2>/dev/null; then
+        easy_install -q --user pip
+    else
+        echo "Both easy_install and pip are missing; please install pip."
+        echo "https://pip.pypa.io/en/stable/installing/"
+        exit 1
+    fi
+    echo "Installing Ansible..."
+    pip install ansible -q --upgrade --user
+fi
+
 echo "Configuring the Ubersmith installer for an existing installation..."
 ansible-playbook -i ./hosts -c local configure.yml
