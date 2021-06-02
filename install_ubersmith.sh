c7548a159e9ae3f8c42826180c5d6b513274f0a0
Michael Styne <mstyne@ubersmith.com>
2021-06-02 12:36:04 -0400

    - easy_install is a python 2 relic, RIP

--- install_ubersmith.sh
+++ install_ubersmith.sh
@@ -2,26 +2,21 @@
 set -e
 
 # SSL and Python development libraries are required.
-# sudo yum install gcc libffi-devel python-devel openssl-devel
-# sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+# See documentation at https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility
 
 export PYTHONUSERBASE="$HOME/.local"
 export PATH="$HOME/.local/bin:$PATH"
 
 echo "Checking for pip3..."
 if command -v pip3 >/dev/null 2>&1; then
+    echo "Installing contemporary version of pip3..."
+    pip3 install --user --force pip==20.3.4
     echo "Installing Ansible..."
     pip3 install "ansible>=2.9,<2.10" -q --upgrade --user --force
 else
-    if command -v easy_install 2>/dev/null; then
-        easy_install -q --user pip==20.3.4
-    else
-        echo "Both easy_install and pip3 are missing; please install pip3."
-        echo "https://pip3.pypa.io/en/stable/installing/"
-        exit 1
-    fi
-    echo "Installing Ansible..."
-    pip3 install "ansible>=2.9,<2.10" -q --upgrade --user --force 
+    echo "The pip3 utility is missing; please install pip3."
+    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
+    exit 1
 fi
 echo "Installing Ubersmith..."
 ansible-playbook -i ./hosts -c local --skip-tags upgrade_only install_ubersmith.yml
