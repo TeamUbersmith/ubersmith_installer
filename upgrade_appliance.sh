a4bc42e53602df6da9e67876d2faa0c6f352e59c
Michael Styne <mstyne@ubersmith.com>
2021-06-02 12:38:15 -0400

    - use the version of pip we've just installed

--- upgrade_appliance.sh
+++ upgrade_appliance.sh
@@ -12,7 +12,7 @@ if command -v pip3 >/dev/null 2>&1; then
     echo "Installing contemporary version of pip3..."
     pip3 install --user --force pip==20.3.4
     echo "Installing Ansible..."
-    pip3 install "ansible>=2.9,<2.10" -q --upgrade --user --force
+    $HOME/.local/bin/pip3 install "ansible>=2.9,<2.10" -q --upgrade --user --force
 else
     echo "The pip3 utility is missing; please install pip3."
     echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
