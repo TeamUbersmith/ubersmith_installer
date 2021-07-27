61d22554bd837d1836217830cd8ddc9e9104be96
Michael Styne <mstyne@ubersmith.com>
2021-07-27 12:54:24 -0400

    - latest 2.10 - uninstall ansible first

--- upgrade_appliance.sh
+++ upgrade_appliance.sh
@@ -12,7 +12,8 @@ if command -v pip3 >/dev/null 2>&1; then
     echo "Installing contemporary version of pip3..."
     pip3 install -q --user --force pip
     echo "Installing Ansible..."
-    $HOME/.local/bin/pip3 install "ansible==2.10" -q --upgrade --user --force
+    $HOME/.local/bin/pip3 uninstall ansible
+    $HOME/.local/bin/pip3 install "ansible>=2.10,<2.11" -q --upgrade --user --force
 else
     echo "The pip3 utility is missing; please install pip3."
     echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
