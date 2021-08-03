da84751a5399ebed165582fb63dada51aea976f5
Michael Styne <mstyne@ubersmith.com>
2021-08-03 15:37:43 -0400

    - install 4.x

--- upgrade_appliance.sh
+++ upgrade_appliance.sh
@@ -13,7 +13,7 @@ if command -v pip3 >/dev/null 2>&1; then
     pip3 install -q --user --force pip
     echo "Installing Ansible..."
     $HOME/.local/bin/pip3 uninstall ansible -y
-    $HOME/.local/bin/pip3 install "ansible>=2.10,<2.11" -q --upgrade --user --force
+    $HOME/.local/bin/pip3 install "ansible>=4,<5" -q --upgrade --user --force
 else
     echo "The pip3 utility is missing; please install pip3."
     echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
