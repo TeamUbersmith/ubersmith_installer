9c3d370db3aca191b18bb02c31e4f11183bf755e
Michael Styne <mstyne+github@ubersmith.com>
2021-08-23 08:08:04 -0400

    - replace retry letsencrypt script

---
+++ retry_letsencrypt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# SSL and Python development libraries are required.
+# See documentation at https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility
+
+export PYTHONUSERBASE="$HOME/.local"
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "Retrying Let's Encrypt certificate request..."
+ansible-playbook -i ./hosts -c local -t certbot install_ubersmith.yml
