b2f1887735b9a9e31f3b7e339c9c3a6b268eb6c9
Michael Styne <mstyne+github@ubersmith.com>
2021-09-08 11:13:31 -0400

    - commit to the correct branch

--- configure.sh
+++ configure.sh
@@ -6,19 +6,19 @@ export PATH="$HOME/.local/bin:$PATH"
 
 echo "Checking for pip3..."
 if command -v pip3 >/dev/null 2>&1; then
+    echo "Installing contemporary version of pip3..."
+    pip3 install -q --user --upgrade pip
     echo "Installing Ansible..."
-    pip3 install "ansible>=2.9,<2.10" -q --upgrade --user
+    $HOME/.local/bin/pip3 uninstall ansible -y
+    $HOME/.local/bin/pip3 uninstall ansible-base -y
+    $HOME/.local/bin/pip3 install "ansible-core>=2.11,<2.12" -q --upgrade --user
 else
-    if command -v easy_install 2>/dev/null; then
-        easy_install -q --user pip==20.3.4
-    else
-        echo "Both easy_install and pip3 are missing; please install pip3."
-        echo "https://pip3.pypa.io/en/stable/installing/"
-        exit 1
-    fi
-    echo "Installing Ansible..."
-    pip3 install ansible -q --upgrade --user
+    echo "The pip3 utility is missing; please install pip3."
+    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
+    exit 1
 fi
+echo "Installing Dependencies..."
+ansible-galaxy install -r requirements.yml
 
 echo "Configuring the Ubersmith installer for an existing installation..."
 ansible-playbook -i ./hosts -c local configure.yml
