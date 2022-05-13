bb3616ec4b36e7bfb5969ba6ffc10af292dd6a17
Michael Styne <mstyne+github@ubersmith.com>
2022-05-13 11:52:30 -0400

    - remove interactive check

--- patch_ubersmith.sh
+++ patch_ubersmith.sh
@@ -12,7 +12,7 @@ if command -v pip3 >/dev/null 2>&1; then
     echo "Installing contemporary version of pip3..."
     pip3 install -q --user --upgrade pip
     echo "Installing jmespath..."
-    $HOME/.local/bin/pip3 install jmespath -q --upgrade --user --force-reinstall
+    $HOME/.local/bin/pip3 install jmespath -q --upgrade --user
 else
     echo "The pip3 utility is missing; please install pip3."
     echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
