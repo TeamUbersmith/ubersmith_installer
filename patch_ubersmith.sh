cabf7a77f5dd9e8fae43542f4b8ea33d63bfb18e
Michael Styne <mstyne+github@ubersmith.com>
2022-05-13 09:57:27 -0400

    - install jmespath

--- patch_ubersmith.sh
+++ patch_ubersmith.sh
@@ -7,5 +7,17 @@ set -e
 export PYTHONUSERBASE="$HOME/.local"
 export PATH="$HOME/.local/bin:$PATH"
 
+echo "Checking for pip3..."
+if command -v pip3 >/dev/null 2>&1; then
+    echo "Installing contemporary version of pip3..."
+    pip3 install -q --user --upgrade pip
+    echo "Installing jmespath..."
+    $HOME/.local/bin/pip3 install jmespath -q --upgrade --user --force-reinstall
+else
+    echo "The pip3 utility is missing; please install pip3."
+    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
+    exit 1
+fi
+
 echo "Patching Ubersmith..."
 ansible-playbook -i ./hosts -c local patch_ubersmith.yml
