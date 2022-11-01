76c7e4f4efb64e4e65ad0d6e2505602b81f724a7
Michael Styne <mstyne+github@ubersmith.com>
2022-11-01 12:01:46 -0400

    Patch (#8)
    * - can search patches based on ubersmith version
    
    * - name, id, link, asset link
    
    * - keep trying to build prompt
    
    * - add in-progress patch application playbook
    
    * - create patch directory
    - try to download a patch :eyes:
    
    * - define ubersmith home
    
    * - install jmespath
    
    * - need to request patch assets first
    
    * - try building url outside of set_fact
    
    * - "fix" linter
    - try to download and decompress patch
    
    * - reference list variable properly, hopefully
    
    * - time to debug :(
    
    * - fix asset variable
    
    * - write out patch data
    
    * - ansible_user_id, not ansible_user
    
    * - get the list of files unarchived onto the system
    
    * - check for .patched, but allow to proceed
    
    * - remove interactive check
    
    * - restart the web container to load the patches
    
    * - we're not failing, we're pausing
    
    * - per patch directory
    
    * - directory must exist first
    
    * - add the ability to remove patches
    
    * - commentary, I guess

---
+++ remove_patches.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# SSL and Python development libraries are required.
+# See documentation at https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility
+
+export PYTHONUSERBASE="$HOME/.local"
+export PATH="$HOME/.local/bin:$PATH"
+
+echo "Checking for pip3..."
+if command -v pip3 >/dev/null 2>&1; then
+    echo "Installing contemporary version of pip3..."
+    pip3 install -q --user --upgrade pip
+    echo "Installing jmespath..."
+    $HOME/.local/bin/pip3 install jmespath -q --upgrade --user
+else
+    echo "The pip3 utility is missing; please install pip3."
+    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
+    exit 1
+fi
+
+echo "Patching Ubersmith..."
+ansible-playbook -i ./hosts -c local -t remove_patches,restart patch_ubersmith.yml
