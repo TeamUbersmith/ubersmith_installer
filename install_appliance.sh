60b8d8622410617994dac6b5ff406af11fd25bd8
Michael Styne <mstyne+github@ubersmith.com>
2023-02-02 07:29:57 -0500

    Automatically configure the XML-RPC user credentials (#9)
    * - fix redis container name
    - don't use symlinks to apply patches
    
    * - make sure /app/patches gets created
    
    * - update certbot renewal script
    - fix path for patch files
    - add volumes for patch directory
    
    * - update xml-rpc user/password
    
    * - add community.mysql dependency
    
    * - login, not user
    
    * - make the debug output stand out a bit more
    
    * - display the right password
    - oy
    
    * - will generate a new password every time
    - should have realized that
    
    * -
    
    * - reformat credential output
    
    * - add missing "

--- install_appliance.sh
+++ install_appliance.sh
@@ -14,6 +14,7 @@ if command -v pip3 >/dev/null 2>&1; then
     echo "Installing Ansible..."
     $HOME/.local/bin/pip3 uninstall ansible -y
     $HOME/.local/bin/pip3 install "ansible-core>=2.11,<2.12" -q --upgrade --user --force-reinstall
+    $HOME/.local/bin/pip3 install pymysql -q --upgrade --user --force-reinstall
 else
     echo "The pip3 utility is missing; please install pip3."
     echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
