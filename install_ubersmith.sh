3c65ce633877e62aef74f32147f4cc87f1678020
Michael Styne <mstyne+github@ubersmith.com>
2023-07-27 14:26:22 -0400

    - beginnings of a python virtualenv

--- install_ubersmith.sh
+++ install_ubersmith.sh
@@ -5,21 +5,17 @@ set -e
 # See documentation at https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility
 
 export PYTHONUSERBASE="$HOME/.local"
-export PATH="$HOME/.local/bin:$PATH"
 
-echo "Checking for pip3..."
-if command -v pip3 >/dev/null 2>&1; then
-    echo "Installing contemporary version of pip3..."
-    pip3 install -q --user --upgrade pip
-    echo "Installing Ansible..."
-    $HOME/.local/bin/pip3 uninstall ansible -q -y
-    $HOME/.local/bin/pip3 install "ansible-core>=2.11,<2.12" -q --upgrade --user --force-reinstall
-    $HOME/.local/bin/pip3 install --upgrade --user pyopenssl
-else
-    echo "The pip3 utility is missing; please install pip3."
-    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
-    exit 1
-fi
+echo "Installing Python virtualenv..."
+python -m pip install -q --user virtualenv
+
+echo "Creating Ubersmith Python virtualenv..."
+$HOME/.local/bin/virtualenv -q $HOME/.local/ubersmith_venv
+source $HOME/.local/ubersmith_venv/bin/activate
+
+echo "Installing Ansible..."
+pip3 install -q "ansible-core>=2.14,<2.15"
+
 echo "Installing Dependencies..."
 ansible-galaxy install -r requirements.yml
 echo "Installing Ubersmith..."
