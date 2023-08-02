9c7b2a700397b3994e881d44af7b882fbce2920c
Michael Styne <mstyne+github@ubersmith.com>
2023-08-02 11:38:43 -0400

    - updates for hyphenated container names

--- roles/appliance/files/backup_rrds.sh
+++ roles/appliance/files/backup_rrds.sh
@@ -4,5 +4,5 @@
 
 TIMESTAMP=`date +%Y-%m-%d`
 
-/usr/bin/docker run --rm --volumes-from ubersmith_app_web_1 -v $(pwd):/backup \
+/usr/bin/docker run --rm --volumes-from ubersmith-app_web_1 -v $(pwd):/backup \
   busybox tar cvf /backup/rrd_backup_$TIMESTAMP.tar /var/www/appliance_root/rrds
