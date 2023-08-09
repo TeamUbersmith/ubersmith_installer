1460dbddbeaa4ecd744a1b56621ab49594663e3d
Michael Styne <mstyne+github@ubersmith.com>
2023-08-09 09:19:47 -0400

    - deploy certbot hooks anyway - update helper scripts to use python virtualenv

--- patch_ubersmith.sh
+++ patch_ubersmith.sh
@@ -4,20 +4,12 @@ set -e
 # SSL and Python development libraries are required.
 # See documentation at https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility
 
-export PYTHONUSERBASE="$HOME/.local"
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/.local/ubersmith_venv/bin:$PATH"
 
-echo "Checking for pip3..."
-if command -v pip3 >/dev/null 2>&1; then
-    echo "Installing contemporary version of pip3..."
-    pip3 install -q --user --upgrade pip
-    echo "Installing jmespath..."
-    $HOME/.local/bin/pip3 install jmespath -q --upgrade --user
-else
-    echo "The pip3 utility is missing; please install pip3."
-    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
-    exit 1
-fi
+source $HOME/.local/ubersmith_venv/bin/activate
+
+echo "Installing jmespath..."
+pip3 install -q "jmespath"
 
 echo "Patching Ubersmith..."
 ansible-playbook -i ./hosts -c local --skip-tags remove_patches patch_ubersmith.yml
