f5eec8a694138425350bc7edbef99d00dad605a1
Michael Styne <mstyne+github@ubersmith.com>
2024-01-26 15:17:12 -0500

    - be more specific

--- roles/appliance/files/appliance_start.sh
+++ roles/appliance/files/appliance_start.sh
@@ -2,6 +2,6 @@
 #
 # Stops, removes, and recreates selected base Ubersmith containers.
 
-docker compose -p ubersmith stop
-docker compose -p ubersmith rm -f
+docker compose -p ubersmith stop app_cron app_db app_web
+docker compose -p ubersmith rm -f app_cron app_db app_web
 docker compose -p ubersmith up -d app_cron app_db app_web
