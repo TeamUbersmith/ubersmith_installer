f68a238c9a241b646f63664db57abbbfda16471c
Michael Styne <mstyne+github@ubersmith.com>
2024-02-06 13:07:09 -0500

    - make sure to start rsyslog - make php a dependency of rsyslog

--- roles/ubersmith/files/ubersmith_start.sh
+++ roles/ubersmith/files/ubersmith_start.sh
@@ -2,5 +2,5 @@
 #
 # Recreates selected base Ubersmith containers.
 
-docker compose -p ubersmith up -d cron db mail php solr web 
+docker compose -p ubersmith up -d cron db mail php solr web rsyslog
 docker compose -p ubersmith up --scale redis=3 -d redis
