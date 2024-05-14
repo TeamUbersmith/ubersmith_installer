3550cab67e24aea1725aec0b9d108ef05565c963
Michael Styne <mstyne+github@ubersmith.com>
2024-05-14 10:12:22 -0400

    - remove references to cloud-init - add extra_vars json - use install_ubersmith.yml

---
+++ unattended_ubersmith.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+# SSL and Python development libraries are required.
+# See documentation at https://docs.ubersmith.com/article/ubersmith-installation-and-upgrade-utility-231.html
+
+export PATH="$HOME/.local/bin:$HOME/.local/ubersmith_venv/bin:$PATH"
+
+rm -rf $HOME/.local/ubersmith_venv
+
+# Requires python3-venv on Ubuntu
+echo "Creating Ubersmith Python virtual environment..."
+python -m venv $HOME/.local/ubersmith_venv
+
+source $HOME/.local/ubersmith_venv/bin/activate
+
+echo "Installing Ansible..."
+pip3 install -q "ansible-core>=2.13,<2.15"
+
+echo "Installing Dependencies..."
+ansible-galaxy install -r requirements.yml
+
+echo "Installing Ubersmith..."
+ansible-playbook -i ./hosts -c local --extra-vars "@unattended_parameters.json" --skip-tags upgrade_only install_ubersmith.yml
