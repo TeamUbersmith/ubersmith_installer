97e064c63292fadefeec1227259914ff49622619
Michael Styne <mstyne+github@ubersmith.com>
2024-10-17 09:52:24 -0400

    - use correct pip requirements filename

--- install_appliance.sh
+++ install_appliance.sh
@@ -15,7 +15,7 @@ python3 -m venv $HOME/.local/ubersmith_venv
 source $HOME/.local/ubersmith_venv/bin/activate
 
 echo "Installing Dependencies..."
-pip3 install -q -r pip_requirements.txt
+pip3 install -q -r requirements_pip.txt
 ansible-galaxy install -r requirements_ansible.yml
 
 echo "Installing Ubersmith Appliance..."
