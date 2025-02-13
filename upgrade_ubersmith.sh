e625d043ac6caa2ff157c190e62d15be553229cd
Michael Styne <mstyne+github@ubersmith.com>
2025-02-13 11:41:38 -0500

    - these changes facilitate upgrades on more contemporary OSes

--- upgrade_ubersmith.sh
+++ upgrade_ubersmith.sh
@@ -4,25 +4,19 @@ set -e
 # SSL and Python development libraries are required.
 # See documentation at https://docs.ubersmith.com/article/ubersmith-installation-and-upgrade-utility-231.html
 
-export PYTHONUSERBASE="$HOME/.local"
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/.local/ubersmith_venv/bin:$PATH"
 
-echo "Checking for pip3..."
-if command -v pip3 >/dev/null 2>&1; then
-    echo "Installing contemporary version of pip3..."
-    pip3 install -q --user --upgrade pip
-    echo "Installing Ansible..."
-    $HOME/.local/bin/pip3 uninstall ansible -q -y
-    $HOME/.local/bin/pip3 install -q --user "pyyaml==5.3.1"
-    $HOME/.local/bin/pip3 install "ansible-core>=2.11,<2.12" -q --upgrade --user 
-    $HOME/.local/bin/pip3 install -q --upgrade --user pyopenssl
-else
-    echo "The pip3 utility is missing; please install pip3."
-    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
-    exit 1
-fi
+rm -rf $HOME/.local/ubersmith_venv
+
+# Requires python3-venv on Ubuntu
+echo "Creating Ubersmith Python virtual environment..."
+python3 -m venv $HOME/.local/ubersmith_venv
+
+source $HOME/.local/ubersmith_venv/bin/activate
 
 echo "Installing Dependencies..."
-ansible-galaxy install -r requirements.yml
+pip3 install -q -r requirements_pip.txt
+ansible-galaxy install -r requirements_ansible.yml
+
 echo "Upgrading Ubersmith..."
-ansible-playbook -i ./hosts -c local -t upgrade,upgrade_only upgrade_ubersmith.yml
+ansible-playbook -i ./hosts -e ansible_python_interpreter=$(which python3) -c local -t upgrade,upgrade_only upgrade_ubersmith.yml
