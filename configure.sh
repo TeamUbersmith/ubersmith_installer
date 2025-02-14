2915e9115bca185b2b0c336b48d6da40d166d671
Michael Styne <mstyne+github@ubersmith.com>
2025-02-14 12:53:48 -0500

    - python venv

--- configure.sh
+++ configure.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-export PYTHONUSERBASE="$HOME/.local"
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/.local/ubersmith_venv/bin:$PATH"
 
-echo "Checking for pip3..."
-if command -v pip3 >/dev/null 2>&1; then
-    echo "Installing contemporary version of pip3..."
-    pip3 install -q --user --upgrade pip
-    echo "Installing Ansible..."
-    $HOME/.local/bin/pip3 uninstall ansible -q -y
-    $HOME/.local/bin/pip3 uninstall ansible-base -q -y
-    $HOME/.local/bin/pip3 install "ansible-core>=2.11,<2.12" -q --upgrade --user
-else
-    echo "The pip3 utility is missing; please install pip3."
-    echo "https://docs.ubersmith.com/display/UbersmithDocumentation/Ubersmith+Installation+and+Upgrade+Utility"
-    exit 1
+# Requires python3-venv on Ubuntu
+if [ ! -d "$HOME/.local/ubersmith_venv" ]; then
+    echo "Creating Ubersmith Python virtual environment..."
+    python3 -m venv $HOME/.local/ubersmith_venv
 fi
+
+source $HOME/.local/ubersmith_venv/bin/activate
+
 echo "Installing Dependencies..."
-ansible-galaxy install -r requirements.yml
+pip3 install -q -r requirements_pip.txt
+ansible-galaxy install -r requirements_ansible.yml
 
 echo "Configuring the Ubersmith installer for an existing installation..."
-ansible-playbook -i ./hosts -c local configure.yml
+ansible-playbook -i ./hosts -e ansible_python_interpreter=$(which python3) -c local configure.yml
