07ba3384cf29cc80bb0cdf243243fb6cf810c863
Michael Styne <mstyne+github@ubersmith.com>
2025-06-03 07:10:53 -0400

    - add venv config to remove_patches.sh

--- remove_patches.sh
+++ remove_patches.sh
@@ -6,7 +6,17 @@ set -e
 
 export PATH="$HOME/.local/bin:$HOME/.local/ubersmith_venv/bin:$PATH"
 
+rm -rf $HOME/.local/ubersmith_venv
+
+# Requires python3-venv on Ubuntu
+echo "Creating Ubersmith Python virtual environment..."
+python3 -m venv $HOME/.local/ubersmith_venv
+
 source $HOME/.local/ubersmith_venv/bin/activate
+
+echo "Installing Dependencies..."
+pip3 install -q -r requirements_pip.txt
+ansible-galaxy install -r requirements_ansible.yml
 
 echo "Installing jmespath..."
 pip3 install -q "jmespath"
